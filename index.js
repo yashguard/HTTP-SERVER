10e6402778fc6c814cc92528b5002a68c16b2537
Yash Guard <yashguard>
2023-07-06 20:04:46 +0530

    Except sign up everything is done

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const http = require("http")
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    if(req.url == "/") {
-        res.end("jshebfjsrhbf")
-    } else if(req.url == "/signup") {
-        res.end("jshebfjsrhbf")
-    } if(req.url == "/login") {
-        res.end("jshebfjsrhbf")
-    } if(req.url == "/product") {
-        res.end("jshebfjsrhbf")
-    } else {res.end("Page not found")}
-})
+  console.log(req.url);
+  if (req.url == "/") {
+    fs.readFile("Home.html", "utf-8", (err, data) => {
+      if (err) console.log("Please create the file first before read it");
+      res.end(data);
+    });
+  } else if (req.url == "/signup") {
+    fs.readFile("Signup.html", "utf-8", (err, data) => {
+      if (err) console.log("Please create the file first before read it");
+      res.end(data);
+    });
+  } else if (req.url == "/login") {
+    fs.readFile("Login.html", "utf-8", (err, data) => {
+      if (err) console.log("Please create the file first before read it");
+      res.end(data);
+    });
+  } else if (req.url == "/product") {
+    fs.readFile("Product.html", "utf-8", (err, data) => {
+      if (err) console.log("Please create the file first before read it");
+      res.end(data);
+    });
+  } else {
+    res.end("Page not found");
+  }
+});
 
-server.listen(8010,()=>{
-    console.log("Listening the user")
-})
+server.listen(8020, () => {
+  console.log("Listening the user");
+});
